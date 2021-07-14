95e564e447cb2c6f2477500f8562e43b834864a9
ImpostorEngineer <posta.gereksiz@gmail.com>
2021-07-14 14:56:37 -0400

    CSS and LocalStorage update

--- public/js/scoreboard.js
+++ public/js/scoreboard.js
@@ -15,21 +15,28 @@ let options = {
 let client = new tmi.client(options);
 // Connect the client to the server..
 client.connect();
-let myscore = 0;
-let opponentscore = 0;
+let myscore = +localStorage.getItem('myscore');
+let opponentscore = +localStorage.getItem('opponentscore');
+document.getElementById('opponentscore').innerHTML = opponentscore;
+document.getElementById('myscore').innerHTML = myscore;
+
 client.on('message', (channel, tags, message, self) => {
   let command = message.split(' ');
   if (command[0] == '!win' && tags.badges.moderator != null) {
     myscore += 1;
+    localStorage.setItem('myscore', myscore);
     document.getElementById('myscore').innerHTML = myscore;
   }
   if (command[0] == '!lose' && tags.badges.moderator != null) {
     opponentscore += 1;
+    localStorage.setItem('opponentscore', opponentscore);
     document.getElementById('opponentscore').innerHTML = opponentscore;
   }
   if (command[0] == '!resetscore' && tags.badges.moderator != null) {
     myscore = 0;
     opponentscore = 0;
+    localStorage.setItem('myscore', myscore);
+    localStorage.setItem('opponentscore', opponentscore);
     document.getElementById('myscore').innerHTML = myscore;
     document.getElementById('opponentscore').innerHTML = opponentscore;
   }
