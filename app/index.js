2f280d14208f1abe76dc7cd05e10ebf8ea304b5a
ImpostorEngineer <posta.gereksiz@gmail.com>
2022-03-12 12:18:42 -0500

    fix vercel

--- app/index.js
+++ app/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const api = require('./app/app');
+const api = require('./app');
 const app = express();
 const path = require('path');
 
 app.use(express.static('public'));
-app.use('/', api);
+app.use('/api/', api);
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '../public/index.html');
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
